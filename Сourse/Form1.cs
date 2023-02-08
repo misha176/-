62474444df7b37b992afd8751fa9c84adf877fd3
unknown <misa17600@gmail.com>
2023-02-08 12:16:26 +0200

    Add course

---
+++ Сourse/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+
+using System.Windows.Forms;
+
+namespace Сourse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
